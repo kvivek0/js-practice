64f7f15630d87783e353f8484b922deb85f63c29
Vivek Kumar <vivekksp963@gmail.com>
2026-06-23 14:38:15 +0000

    added var and const

---
+++ Basics/variableAndConst.js
@@ -0,0 +1,9 @@
+const accountId = 4321
+let accountEmail = "vivek@gmail.com"
+var password = "456"
+
+let accountCity = "maharastra"
+ 
+// accountId = 256 
+// console.log(accountId);
+console.table([accountEmail, password, accountCity])
