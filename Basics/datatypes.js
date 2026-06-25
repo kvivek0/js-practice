4ab5f3a19a617b0810bed7011c2c9af8df8da42c
Vivek Kumar <vivekksp963@gmail.com>
2026-06-25 05:27:14 +0000

    added data types

---
+++ Basics/datatypes.js
@@ -0,0 +1,18 @@
+// data types
+
+let score = 243;
+let isWin = true;
+let name = "vivek";
+  
+// number => 2 to the power of 53 (around)
+// bigint        (for big integer value)
+// boolean => true/false
+//  null => standalone value 
+// undefined 
+// symbol ;
+  let score = 546;
+
+// typeof
+ console.log(typeof 23); //
+ console.log(typeof undefined); //undefined 
+ console.log(typeof null);  //object
