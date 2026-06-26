83bcd4965587c164285c1269e65cfca6ba2ff6c5
kvivek0 <vivekksp963@gmail.com]>
2026-06-26 19:54:59 +0530

    added comparison

---
+++ Basics/05_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>3)
+// console.log(2 >= 3)
+// console.log(2 != 3)
+// console.log(2<3)
+// console.log(2 == 3)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+                               // we have to avoid these comparisons;
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+//  "===" :- strict equality operator 
+            // it checks the 1. are the value are equal?
+                        //   2. are the data types are equal?
+console.log("2" === 2);
